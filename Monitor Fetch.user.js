7085e0199677684e61f2a38ba4e1b79884601883
Todd Hambley <thambley@tlcorporate.com>
2024-03-16 14:32:35 -0400

    capture and log additional information

--- Monitor Fetch.user.js
+++ Monitor Fetch.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://support.concurcompleat.com/Logs
 // @downloadURL  https://github.com/thambley/compleat-tampermonkey/raw/main/Monitor%20Fetch.user.js
 // @updateURL    https://github.com/thambley/compleat-tampermonkey/raw/main/Monitor%20Fetch.user.js
-// @version      0.1
+// @version      0.2
 // @description  Watch fetch requests and responses on compleat logs and output information to console log
 // @author       thambley@tlcorporate.com
 // @match        https://support.concurcompleat.com/Logs*
@@ -13,11 +13,53 @@
 
 (function () {
   'use strict';
+  var logs = [];
+  var selectedLogs = [];
+
+  function findLogById(logId) {
+    return logs.find(log => log.id == logId);
+  }
+
+  function jsonHandler(json) {
+    if (json.body != null) {
+      console.log('log id: ' + json.id);
+      console.log('locator: ' + json.name)
+      const found = findLogById(json.id);
+      if (found == null) {
+        logs.push(json);
+        populateSelectedLogs(currentHref);
+      }
+    }
+  }
+
+  function populateSelectedLogs(href) {
+    var queryParts = href.split('?');
+    selectedLogs = [];
+    if (queryParts.length > 1) {
+      var query = queryParts[1].split('&');
+      var parameters = { id: '', sid: [] };
+      for (var i = 0; i < query.length; i++) {
+        var parameterParts = query[i].split('=');
+        if (parameterParts[0] == 'id') {
+          parameters.id = parameterParts[1];
+        } else if (parameterParts[0] == 'sid') {
+          var sid = parameterParts[1];
+          parameters.sid.push(sid);
+          const found = findLogById(sid);
+          if (found != null) {
+            selectedLogs.push(found);
+          }
+        }
+      }
+    }
+    console.log('selected log count: ' + selectedLogs.length);
+  }
 
   // clone the response to be handled by the original handler
   function responseHandler(response) {
     // console.log(response.body);
     var clonedResponse = response.clone();
+    response.json().then(jsonHandler);
     return clonedResponse;
   }
 
@@ -39,6 +81,7 @@
       if (currentHref != document.location.href) {
         currentHref = document.location.href;
         console.log(`currentHref: ${currentHref}`);
+        populateSelectedLogs(currentHref);
       }
     });
   });
